15df9bcf78b94b49982710e074752716e43dd347
wwordyy <vanotim2006@gmail.com>
2026-02-21 22:45:33 +0300

    added service chat

---
+++ backend/services/chatService.js
@@ -0,0 +1,106 @@
+
+
+const prisma = require('../prisma/client');
+
+class ChatService {
+
+
+    async createChat(teacherId, observerId) {
+
+        const existing = await prisma.chatUser.findFirst({
+        where: {
+            userId: observerId,
+            chat: {
+                users: {
+                    some: { userId: teacherId }
+                }
+            }
+        }
+    });
+
+        if (existing) {
+            return existing.chatId;
+        }
+
+        const chat = await prisma.chat.create({
+            data: {
+                users: {
+                    create: [
+                        { userId: observerId },
+                        { userId: teacherId }
+                    ]
+                }
+            }
+        });
+
+        return chat;
+    }
+
+
+    async getChats(userId) {
+
+        const chats = await prisma.chat.findMany({
+            where: {
+                users: { some: { userId } }
+            },
+            include: {
+                users: {
+                    include: { user: { select: { id: true, fullName: true, avatarUrl: true } } }
+                },
+                messages: {
+                    orderBy: { createdAt: 'desc' },
+                    take: 1 
+                }
+            }
+        });
+
+        return chats;
+
+    }
+
+
+
+    async getMessages(chatId, userId) {
+
+        const member = await prisma.chatUser.findUnique({
+            where: {
+                chatId_userId: {
+                    chatId: Number(chatId), userId
+                }
+            }
+
+        });
+
+        if (!member) {
+            throw new Error("Нет доступа к чату!");
+        }
+
+        const messages = await prisma.chatMessage.findMany({
+            where: { chatId: Number(chatId) },
+            include: { author: { select: { id: true, fullName: true, avatarUrl: true } } },
+            orderBy: { createdAt: 'asc' }
+
+        });
+
+
+        return messages;
+    }
+
+
+    async sendMessage(chatId, content, userId) {
+
+        const message = await prisma.chatMessage.create({
+            data: {
+                content,
+                authorId: userId,
+                chatId: Number(chatId)
+            },
+            include: { author: { select: { id: true, fullName: true, avatarUrl: true } } }
+        });
+
+        return message;
+
+    }
+}
+
+module.exports = new ChatService();
