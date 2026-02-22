553d93d375a8e7d8b4c5c837974817130172c259
wwordyy <vanotim2006@gmail.com>
2026-02-22 22:44:34 +0300

    fix router

--- backend/routers/chatRouter.js
+++ backend/routers/chatRouter.js
@@ -13,3 +13,6 @@ router.post('/chats', authMiddleware, roleMiddleware(['observer']), chatControll
 router.get('/chats', authMiddleware, chatController.getChats);
 router.get('/chats/:chatId/messages', authMiddleware, chatController.getMessages);
 router.post('/chats/:chatId/messages', authMiddleware, chatController.sendMessage);
+
+
+module.exports = router;
