5ab3b0349acf0428011ef81d4dbe02c303b22295
wwordyy <vanotim2006@gmail.com>
2026-03-03 14:37:09 +0300

    upd server.js

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,37 @@ app.use(cors({
     credentials: true
 }));  
 
+//shared
 app.use('/api/auth', require('./routers/authRouter'));
-app.use('/api', require('./routers/dashboardRouter'));
 app.use('/api', require('./routers/userRouter'));
-app.use('/api', require('./routers/streamRouter'));
-app.use('/api', require('./routers/groupRouter'));
-app.use('/api', require('./routers/chatRouter'));
+app.use('/api', require('./routers/shared/postCommentRouter'));
+
+
+//observer
+app.use('/api', require('./routers/observer/dashboardRouter'));
+app.use('/api', require('./routers/observer/groupRouter'));
+app.use('/api', require('./routers/observer/chatRouter'));
+app.use('/api', require('./routers/observer/reportRouter'));
+
+
+//admin
+app.use('/api', require('./routers/admin/roleRouter'));
+app.use('/api/admin/users',  require('./routers/admin/userRouter'));
+app.use('/api/admin/groups', require('./routers/admin/groupRouter'));
+
+
+// student
+app.use('/api/student', require('./routers/student/postRouter'));
+app.use('/api/student', require('./routers/student/practiceRouter'));
+
+// teacher
+app.use('/api', require('./routers/teacher/teacherRouter'));
+
+
+
+
+app.use('/uploads', express.static('uploads'));
+
 
 
 
