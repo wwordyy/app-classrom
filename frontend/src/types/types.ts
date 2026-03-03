159ac19c5edf8429e744dcdfb5e2b960cfa59bd6
wwordyy <vanotim2006@gmail.com>
2026-03-03 14:41:18 +0300

    added types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,3 +2,27 @@
 export interface ResponseError {
     error: string
 }
+
+export interface ResponseSuccess {
+    ok: boolean,
+}
+
+export interface User {
+    id: number,
+    fullName: string,
+    email: string,
+    avatarUrl?: string,
+    role?: {
+        title?: string
+    },
+}
+
+
+export interface Group {
+    id: number,
+    name: string,
+    courseYear: number,
+    specialty: string
+
+}
+
