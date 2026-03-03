7fed3840a427334f0620193bda98d7f52e9f96dc
wwordyy <vanotim2006@gmail.com>
2026-03-03 14:40:06 +0300

    upd app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,124 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {Register} from './components/auth/register'
+
+import { Register } from './components/auth/register'
 import { Login } from './components/auth/login'
-import { Dashboard } from './components/observer/dashboard'
+import { RoleRedirect, ProtectedRoute } from './components/auth/roleRedirect'
+
+import { Dashboard } from './components/observer/dashboard/dashboard'
+import { GroupsPage } from './components/observer/group/groupsPage'
+import { ChatsPage } from './components/observer/chats/chatPage'
+import { AssignTeacherPage } from './components/observer/group/assignTeacherPage'
+
+import { TeacherMainPage } from './components/teacher/dashboard/dashboard'
+import { TeacherCreatePostPage } from './components/teacher/post/createPostPage'
+import { PostSubmissionsPage } from './components/teacher/submission/postSubmissionsPage'
+import { TeacherJournalPage } from './components/teacher/journal/journalPage'
+import { TeacherStudentGradesPage } from './components/teacher/journal/studentGradesPage'
+
+import { StudentPostsPage } from './components/student/post/studentPostsPage'
+import { StudentPostDetailPage } from './components/student/post/studentPostDetailPage'
+import { StudentPracticePage } from './components/student/practice/practicePage'
+
+import { AdminGroupsPage } from './components/admin/group/groupPage'
+import { AdminUsersPage } from './components/admin/user/userPage'
 
 function App() {
-
   return (
     <div>
-        <BrowserRouter>
-          <Routes>
+      <BrowserRouter>
+        <Routes>
 
-            <Route path='/register' element={<Register/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/observer/main' element= {<Dashboard/>}/>
+          {/* Публичные */}
+          <Route path='/register' element={<Register />} />
+          <Route path='/login'    element={<Login />} />
+
+          <Route path='/' element={<RoleRedirect />} />
 
 
-          </Routes>
-        
-        </BrowserRouter>
+          {/* Observer */}
+          <Route path='/observer/main' element={
+            <ProtectedRoute allowedRoles={['observer']}>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          <Route path='/observer/groups' element={
+            <ProtectedRoute allowedRoles={['observer']}>
+              <GroupsPage />
+            </ProtectedRoute>
+          } />
+          <Route path='/observer/chats' element={
+            <ProtectedRoute allowedRoles={['observer']}>
+              <ChatsPage />
+            </ProtectedRoute>
+          } />
+          <Route path='/observer/groups/:groupId/assign-teacher' element={
+            <ProtectedRoute allowedRoles={['observer']}>
+              <AssignTeacherPage />
+            </ProtectedRoute>
+          } />
+
+
+          {/* Teacher */}
+          <Route path='/teacher/main' element={
+            <ProtectedRoute allowedRoles={['teacher']}>
+              <TeacherMainPage />
+            </ProtectedRoute>
+          } />
+          <Route path='/teacher/create-post' element={
+            <ProtectedRoute allowedRoles={['teacher']}>
+              <TeacherCreatePostPage />
+            </ProtectedRoute>
+          } />
+          <Route path='/teacher/posts/:postId/submissions' element={
+            <ProtectedRoute allowedRoles={['teacher']}>
+              <PostSubmissionsPage />
+            </ProtectedRoute>
+          } />
+          <Route path='/teacher/journal' element={
+            <ProtectedRoute allowedRoles={['teacher']}>
+              <TeacherJournalPage />
+            </ProtectedRoute>
+          } />
+          <Route path='/teacher/journal/:studentId' element={
+            <ProtectedRoute allowedRoles={['teacher']}>
+              <TeacherStudentGradesPage />
+            </ProtectedRoute>
+          } />
+
+          {/* Student */}
+          <Route path='/student/posts' element={
+            <ProtectedRoute allowedRoles={['student']}>
+              <StudentPostsPage />
+            </ProtectedRoute>
+          } />
+
+         <Route path='/student/posts/:postId' element={
+              <ProtectedRoute allowedRoles={['student']}>
+                  <StudentPostDetailPage />
+              </ProtectedRoute>
+          } />
+
+          <Route path='/student/practice' element={
+              <ProtectedRoute allowedRoles={['student']}>
+                  <StudentPracticePage />
+              </ProtectedRoute>
+          } />
+
+
+          {/* Admin */}
+          <Route path='/admin/users' element={
+            <ProtectedRoute allowedRoles={['admin']}>
+              <AdminUsersPage />
+            </ProtectedRoute>
+          } />
+          <Route path='/admin/groups' element={
+            <ProtectedRoute allowedRoles={['admin']}>
+              <AdminGroupsPage />
+            </ProtectedRoute>
+          } />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
