77f8c5862f50fb01c5a31f4eef9eadf7c0f88481
wwordyy <vanotim2006@gmail.com>
2026-03-05 19:36:55 +0300

    upd app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,21 @@ import { Dashboard } from './components/observer/dashboard/dashboard'
 import { GroupsPage } from './components/observer/group/groupsPage'
 import { ChatsPage } from './components/observer/chats/chatPage'
 import { AssignTeacherPage } from './components/observer/group/assignTeacherPage'
+import { ObserverProfilePage } from './components/observer/profile/observerProfile'
 
 import { TeacherMainPage } from './components/teacher/dashboard/dashboard'
 import { TeacherCreatePostPage } from './components/teacher/post/createPostPage'
 import { PostSubmissionsPage } from './components/teacher/submission/postSubmissionsPage'
 import { TeacherJournalPage } from './components/teacher/journal/journalPage'
 import { TeacherStudentGradesPage } from './components/teacher/journal/studentGradesPage'
+import { TeacherChatsPage } from './components/teacher/chats/teacherChats'
+import { TeacherProfilePage } from './components/teacher/profile/teacherProfile'
 
 import { StudentPostsPage } from './components/student/post/studentPostsPage'
 import { StudentPostDetailPage } from './components/student/post/studentPostDetailPage'
 import { StudentPracticePage } from './components/student/practice/practicePage'
+import { StudentProfilePage } from './components/student/profile/studentProfile'
+import { StudentChatsPage } from './components/student/chats/studentChats'
 
 import { AdminGroupsPage } from './components/admin/group/groupPage'
 import { AdminUsersPage } from './components/admin/user/userPage'
@@ -41,21 +46,30 @@ function App() {
               <Dashboard />
             </ProtectedRoute>
           } />
+
           <Route path='/observer/groups' element={
             <ProtectedRoute allowedRoles={['observer']}>
               <GroupsPage />
             </ProtectedRoute>
           } />
+
           <Route path='/observer/chats' element={
             <ProtectedRoute allowedRoles={['observer']}>
               <ChatsPage />
             </ProtectedRoute>
           } />
+
           <Route path='/observer/groups/:groupId/assign-teacher' element={
             <ProtectedRoute allowedRoles={['observer']}>
               <AssignTeacherPage />
             </ProtectedRoute>
           } />
+
+        <Route path='/observer/profile' element={
+            <ProtectedRoute allowedRoles={['observer']}>
+                <ObserverProfilePage />
+            </ProtectedRoute>
+        } />
 
 
           {/* Teacher */}
@@ -63,6 +77,11 @@ function App() {
             <ProtectedRoute allowedRoles={['teacher']}>
               <TeacherMainPage />
             </ProtectedRoute>
+          } />
+          <Route path='/teacher/chats' element={
+              <ProtectedRoute allowedRoles={['teacher']}>
+                  <TeacherChatsPage />
+              </ProtectedRoute>
           } />
           <Route path='/teacher/create-post' element={
             <ProtectedRoute allowedRoles={['teacher']}>
@@ -84,6 +103,12 @@ function App() {
               <TeacherStudentGradesPage />
             </ProtectedRoute>
           } />
+          <Route path='/teacher/profile' element={
+              <ProtectedRoute allowedRoles={['teacher']}>
+                  <TeacherProfilePage />
+              </ProtectedRoute>
+          } />
+
 
           {/* Student */}
           <Route path='/student/posts' element={
@@ -102,6 +127,17 @@ function App() {
               <ProtectedRoute allowedRoles={['student']}>
                   <StudentPracticePage />
               </ProtectedRoute>
+          } />
+
+          <Route path='/student/profile' element={
+              <ProtectedRoute allowedRoles={['student']}>
+                  <StudentProfilePage />
+                </ProtectedRoute>
+          } />
+          <Route path='/student/chats' element={
+              <ProtectedRoute allowedRoles={['student']}>
+                  <StudentChatsPage />
+                </ProtectedRoute>
           } />
 
 
