c6917ee6be051d61df85a0bb23a710c9717b270a
wwordyy <vanotim2006@gmail.com>
2026-03-07 16:02:16 +0300

    fix router

--- backend/routers/shared/chatRouter.js
+++ backend/routers/shared/chatRouter.js
@@ -9,7 +9,7 @@ const roleMiddleware = require('../../middleware/roleMiddleware');
 const chatController = require('../../controllers/shared/chatController');
 
 
-router.post('/chats', authMiddleware, roleMiddleware(['observer']), chatController.createChat);
+router.post('/chats', authMiddleware, roleMiddleware(['observer', 'teacher']), chatController.createChat);
 router.get('/chats', authMiddleware, chatController.getChats);
 router.get('/chats/:chatId/messages', authMiddleware, chatController.getMessages);
 router.post('/chats/:chatId/messages', authMiddleware, chatController.sendMessage);
